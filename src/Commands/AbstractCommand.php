c5d20e37621abd986bf82b2af1a83b281d5b77f2
Ivan Kovbas <kovbas.ivan@gmail.com>
2015-06-28 01:02:22 +0300

    [New Feature] : Check if environment was saved  - Add checking if the environment was saved before switching to other environment  - Refactoring

--- src/Commands/AbstractCommand.php
+++ src/Commands/AbstractCommand.php
@@ -6,23 +6,58 @@ use Illuminate\Support\Facades\File;
 class AbstractCommand extends Command{
 
     /**
+     * Path to .env file
+     *
+     * @var string
+     */
+    protected $dotenv_path;
+
+    /**
+     * Current environment name
+     *
+     * @var string
+     */
+    protected $current_env;
+
+    /**
+     * @var string
+     */
+    protected $current_env_file_name;
+
+    /**
+     * @var string
+     */
+    protected $current_env_file_path;
+
+    protected $messages = [
+        'save.success'              => 'Environmental config was successfully saved to <comment>%s</comment>',
+        'save.ask'                  => 'You have unsaved changes for environment "%s", would you like to save changes before switching? [Y, N]',
+        'switch.success'            => 'Successfully switched from <comment>%s</comment> to <comment>%s</comment>',
+        'switch.error.not_exist'    => 'Cannot switch to environment:<info> %s </info>because<info> %s </info>doesn\'t exist'
+    ];
+
+    public function __construct() {
+        parent::__construct();
+
+        // Determinate path to .env file
+        $this->dotenv_path              = getcwd() . '/.env';
+
+        // Determinate current environment data
+        $this->current_env              = getenv('APP_ENV');
+        $this->current_env_file_name    = $this->getEnvFileName($this->current_env);
+        $this->current_env_file_path    = getcwd() . '/' . $this->current_env_file_name;
+    }
+
+    /**
      * Save current environment to custom file
      */
     protected function saveEnvironment() {
 
-        // Determinate path to .env file
-        $dotenv_path            = getcwd() . '/.env';
-
-        // Determinate current environment and file name for it
-        $current_env            = getenv('APP_ENV');
-        $current_env_file_name  = $this->getEnvFileName($current_env);
-        $current_env_file_path  = getcwd() . '/' . $current_env_file_name;
-
         // Save current environment to custom file
-        $this->copyFile($dotenv_path, $current_env_file_path);
+        $this->copyFile($this->dotenv_path, $this->current_env_file_path);
 
         // Show message
-        $this->info('Environmental config was successfully saved to <comment>' . $current_env_file_name . '</comment>');
+        $this->info(sprintf($this->messages['save.success'], $this->current_env_file_name));
     }
 
     /**
@@ -32,30 +67,57 @@ class AbstractCommand extends Command{
      */
     protected function switchEnvironment() {
 
-        // Determinate path to .env file
-        $dotenv_path            = getcwd() . '/.env';
-
-        // Determinate current
-        $current_env            = getenv('APP_ENV');
-
         // Determinate target environment and file name for it
         $target_env             = $this->argument('env');
         $target_env_file_name   = $this->getEnvFileName($target_env);
         $target_env_file_path   = getcwd() . '/' . $target_env_file_name;
 
-        // TODO: Add a checking, if current environment was saved
+        // Check if .env was saved to the custom .env.$APP_ENV file
+        if (!$this->isEnvironmentSaved()) {
+            $this->askToSave();
+        }
 
         // Check if target env file exist
         if (!File::exists($target_env_file_path)) {
-            $this->error('Cannot switch to environment:<info> ' . $target_env . ' </info>because<info> ' . $target_env_file_name .' </info>doesn\'t exist');
+            $this->error(sprintf($this->messages['switch.error.not_exist'], $target_env, $target_env_file_name));
             return;
         }
 
         // Copy target env data to .env file
-        $this->copyFile($target_env_file_path, $dotenv_path);
+        $this->copyFile($target_env_file_path, $this->dotenv_path);
 
         // Show message
-        $this->info('Successfully switched from <comment>' . $current_env . '</comment> to <comment>' . $target_env . '</comment>');
+        $this->info(sprintf($this->messages['switch.success'], $this->current_env, $target_env));
+    }
+
+    /**
+     * Ask user if he want to save env and save it
+     */
+    protected function askToSave() {
+
+        // Ask user if he wants to save environment
+        $answer = $this->askWithCompletion(sprintf($this->messages['save.ask'], $this->current_env), ["Y", "N"], "Y");
+
+        // Save environment if yes
+        if ($answer == "Y") {
+            $this->saveEnvironment();
+        }
+    }
+
+    /**
+     * Method check if the current environment is saved to custom file
+     *
+     * @return bool
+     */
+    protected function isEnvironmentSaved() {
+
+        // Check if custom env file exists
+        if (!File::exists($this->current_env_file_path)) {
+            return false;
+        }
+
+        // Check if content of .env is equals to .env.$APP_ENV
+        return (File::get($this->dotenv_path) == File::get($this->current_env_file_path));
     }
 
     /**
